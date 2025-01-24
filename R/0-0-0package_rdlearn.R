c690bab6c61f1bf2be255f58434acd38bded66ab
kkawato <kentaro1358nohe@gmail.com>
2025-01-24 11:17:26 +0900

    modify example to reduce runtime

--- R/0-0-0package_rdlearn.R
+++ R/0-0-0package_rdlearn.R
@@ -46,7 +46,28 @@
 #'
 #' @examples
 #' # Load the simulation data
-#' data(simdata_A)
+#'
+#' # Simulation Data A from Appendix D of Zhang et al. (2022)
+#' n <- 200
+#' X <- runif(n, -1000, -1)
+#' G <- 2 * as.numeric(
+#' I(0.01 * X + 5 + rnorm(n, sd = 10) > 0)
+#' ) +
+#' as.numeric(
+#' I(0.01 * X + 5 + rnorm(n, sd = 10) <= 0)
+#' )
+#' c1 <- -850
+#' c0 <- -571
+#' C <- ifelse(G == 1, c1, c0)
+#' D <- as.numeric(X >= C)
+#' coef0 <- c(-1.992230e+00, -1.004582e-02, -1.203897e-05, -4.587072e-09)
+#' coef1 <- c(9.584361e-01, 5.308251e-04, 1.103375e-06, 1.146033e-09)
+#' Px <- poly(X - 735.4334 - c1, degree = 3, raw = TRUE) # -735.4334-c1 = 164.43
+#' Px <- cbind(rep(1, nrow(Px)), Px)
+#' EY0 <- Px %*% coef0
+#' EY1 <- Px %*% coef1
+#' d <- 0.2 + exp(0.01 * X) * (1 - G) + 0.3 * (1 - D)
+#' Y <- EY0 * (1 - D) + EY1 * D - d * as.numeric(I(G == 1)) + rnorm(n, sd = 0.3)
 #'
 #' # Learn new treatment assignment cutoffs
 #' rdlearn_result <- rdlearn(
@@ -59,6 +80,8 @@
 #'
 #' # Visualize the learned policies
 #' plot(rdlearn_result, opt = "dif")
+#' # The learned cutoff for Group 1 is the same as the baseline cutoff, because
+#' # the baseline cutoff is set to equal to oracle cutoff in this simulation.
 #'
 #' # Implement sensitivity analysis
 #' sens_result <- sens(rdlearn_result, M = 1, cost = c(0, 1))
